2dd4b67d69939472339ae02858db2a01255fe002
fernandezmayra <fernandezmayra239@gmail.com>
2022-12-24 02:36:42 -0300

    modificación webconfig

--- src/main/java/com/portfolio/mcfs/WebConfig.java
+++ src/main/java/com/portfolio/mcfs/WebConfig.java
@@ -11,6 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**"); 
+        registry.addMapping("/**") 
+        .allowedOrigins("https://miportfolio-1.web.app")
+        .allowedMethods("*")
+        .allowedHeaders("*");
     }
 }
